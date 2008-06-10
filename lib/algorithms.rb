fd478ba33ec90e4dd4f9c1694954128a2667878d
Kanwei Li <kanwei@gmail.com>
2008-06-10 17:47:50 -0400

    Added Queue

--- lib/algorithms.rb
+++ lib/algorithms.rb
@@ -1,6 +1,5 @@
-require "#{File.dirname(__FILE__)}/datastructures/heap"
-require "#{File.dirname(__FILE__)}/datastructures/stack"
+%w(heap stack queue priority_queue).each { |file| require "#{File.dirname(__FILE__)}/datastructures/#{file}" }
 
 class Algorithms
-  VERSION = '1.0.0'
+  VERSION = '0.0.1'
 end
--- lib/datastructures/stack.rb
+++ lib/datastructures/stack.rb
@@ -1,27 +1,29 @@
-# Use a Ruby array for storage
-class Stack
-  def initialize(ary=[])
-    @container = Array.new
-  end
+class DS
+  # Use a Ruby array for storage
+  class Stack
+    def initialize(ary=[])
+      @container = Array.new
+    end
   
-  def push(object)
-    @container.push(object)
-  end
+    def push(object)
+      @container.push(object)
+    end
   
-  def peek
-    @container.last
-  end
+    def peek
+      @container.last
+    end
   
-  def pop
-    @container.pop
-  end
+    def pop
+      @container.pop
+    end
   
-  def size
-    @container.size
-  end
+    def size
+      @container.size
+    end
   
-  def empty?
-    @container.empty?
-  end
+    def empty?
+      @container.empty?
+    end
   
+  end
 end
