ecbca93808a625eb44d38f8ddb65ef57c11e7475
Kanwei Li <kanwei@gmail.com>
2008-06-10 17:15:15 -0400

    Lets you merge with other heaps and supports initialization with an array

--- spec/heap_spec.rb
+++ spec/heap_spec.rb
@@ -13,7 +13,7 @@ describe DS::Heap do
       @heap.get_max!.should eql(nil)
     end
     
-    it "should let you insert" do
+    it "should let you insert and remove one item" do
       @heap.size.should eql(0)
       
       @heap.insert(1)
@@ -22,15 +22,24 @@ describe DS::Heap do
       @heap.get_max!.should eql(1)
       @heap.size.should eql(0)
     end
+    
+    it "should let you initialize with an array" do
+      @heap = DS::Heap.new([1,2,3])
+      @heap.size.should eql(3)
+    end
+
   end
   
   describe "(non-empty)" do
     before(:each) do
       @random_array = []
-      @num_items = 1000
+      @num_items = 100
       @num_items.times { |x| @random_array << rand(@num_items) }
-      @heap = DS::Heap.new
-      @random_array.each { |n| @heap.insert(n) }
+      @heap = DS::Heap.new(@random_array)
+    end
+    
+    it "should display the correct size" do
+      @heap.size.should eql(@num_items)
     end
     
     it "should be correct!" do
@@ -40,6 +49,18 @@ describe DS::Heap do
       end
       
       ordered.should eql(@random_array.sort.reverse)
+    end
+    
+    it "should let you merge with another heap" do
+      numbers = [1,3,4,5]
+      otherheap = DS::Heap.new(numbers)
+      otherheap.size.should eql(4)
+      @heap.merge!(otherheap)
+      
+      ordered = []
+      ordered << @heap.get_max! until @heap.size == 0
+      
+      ordered.should eql( (@random_array + numbers).sort.reverse)
     end
     
     it "should be faster than the naive approach of finding maximum in an array" do
