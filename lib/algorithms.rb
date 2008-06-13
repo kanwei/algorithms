8a840bf6bb76a567efa5cb1f9557b995c403eb98
Kanwei Li <Kanwei@anon235.mathcs.emory.edu>
2008-06-12 22:43:04 -0400

    Implemented Red-Black Tree

--- lib/algorithms.rb
+++ lib/algorithms.rb
@@ -1,4 +1,4 @@
-%w(heap stack queue priority_queue).each { |file| require "#{File.dirname(__FILE__)}/datastructures/#{file}" }
+%w(heap stack queue priority_queue tree).each { |file| require "#{File.dirname(__FILE__)}/datastructures/#{file}" }
 
 class Algorithms
   VERSION = '0.0.1'
---
+++ spec/tree_spec.rb
@@ -0,0 +1,58 @@
+require 'lib/algorithms'
+require 'benchmark'
+include Benchmark
+
+describe DS::RedBlackTree do
+  before(:each) do
+    @tree = DS::RedBlackTree.new
+  end
+  
+  describe "(empty)" do
+    it "should let you put stuff in" do
+      100.times { |x| @tree.put(x, x) }
+      @tree.size.should eql(100)
+    end
+    
+    it "should return 0 for height" do
+      @tree.height.should eql(0)
+    end
+    
+    it "should return 0 for size" do
+      @tree.size.should eql(0)
+    end
+    
+    it "should return nil for min and max" do
+      @tree.min.should eql(nil)
+      @tree.max.should eql(nil)
+    end
+    
+  end
+  
+  describe "(non-empty)" do
+    before(:each) do
+      @num_items = 100
+      @random_array = []
+      @num_items.times { @random_array << rand(@num_items) }
+      @random_array.each { |x| @tree.put(x, x) }
+    end
+    
+    it "should return correct size (uniqify items first)" do
+      @tree.size.should eql(@random_array.uniq.size)
+    end
+    
+    it "should return correct max and min" do
+      @tree.min.should eql(@random_array.min)
+      @tree.max.should eql(@random_array.max)
+    end
+    
+    it "should not #contain? keys it doesn't have" do
+      @tree.contains?(10000).should eql(false)
+    end
+    
+    it "should #contain? keys it does have" do
+      @tree.contains?(@random_array[0]).should eql(true)
+    end
+    
+  end
+  
+end
