15237f42e9dc91614e1299679bde53eaa9e7ba76
Kanwei Li <kanwei@gmail.com>
2008-07-10 19:29:22 -0400

    Update the benchmarks (still not very useful)

--- benchmark.rb
+++ benchmark.rb
@@ -1,4 +1,7 @@
-require 'lib/algorithms'
+# :nodoc: all
+$: << File.join(File.expand_path(File.dirname(__FILE__)), 'lib')
+require 'algorithms'
+
 require 'benchmark'
 include Benchmark
 
@@ -6,44 +9,35 @@ include Benchmark
 @random_array = []
 @num_items = 10000
 @num_items.times { |x| @random_array << rand(@num_items) }
-# @heap = DS::MaxHeap.new(@random_array)
-
+# @heap = Containers::MaxHeap.new(@random_array)
+# 
 # benchmark do |bench|
 #   bench.report("Array#max:\t ") do
 #     @num_items.times { @random_array.delete_at(@random_array.index(@random_array.max)) }
 #   end
 #   bench.report("MaxHeap:\t ") do
-#     @num_items.times { @heap.get_max! }
+#     @num_items.times { @heap.max! }
 #   end
 # end
 
 # Benchmark Search trees
-@tree = Containers::TreeMap.new
-@ctree = Containers::TreeMap.new
+@rb_tree = Containers::RubyRBTreeMap.new
+@splay_tree = Containers::SplayTreeMap.new
 @hash = Hash.new
 
 benchmark do |bench|
-  bench.report("Tree: \t") do
-    @random_array.each { |x| @tree[x] = x }
-  end
+  puts "\nInsertion"
+  bench.report("RBTree: \t")    { @random_array.each  { |x| @rb_tree[x] = x } }
+  bench.report("SplayTree: \t") { @random_array.each  { |x| @splay_tree[x] = x } }
+  bench.report("Hash: \t\t")    { @hash.each          { |x| @hash[x] = x } }
   
-  bench.report("CTree: \t") do
-    @random_array.each { |x| @ctree[x] = x }
-  end
+  puts "\nTest has_key?" 
+  bench.report("RBTree: \t")    { @num_items.times { |n| @rb_tree.has_key?(n) } }
+  bench.report("SplayTree: \t") { @num_items.times { |n| @splay_tree.has_key?(n) } }
+  bench.report("Hash: \t\t")    { @num_items.times { |n| @hash.has_key?(n) } }
   
-  bench.report("Hash: \t") do
-    @hash.each { |x| @hash[x] = x }
-  end
-
-  bench.report("Hash: \t") do
-    @num_items.times { |n| @hash.has_key?(n) }
-  end
-  
-  bench.report("Tree: \t") do
-    @num_items.times { |n| @tree.contains?(n) }
-  end
-  
-  bench.report("CTree: \t") do
-    @num_items.times { |n| @ctree.contains?(n) }
-  end
+  puts "\nSorted order" 
+  bench.report("RBTree: \t")    { @rb_tree.each { |k, v| k } }
+  bench.report("SplayTree: \t") { @splay_tree.each { |k, v| k } }
+  bench.report("Hash: \t\t")    { @hash.sort.each { |k, v| k } }
 end
