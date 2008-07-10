32bb072e5c79d4825cce562c70f40741b9aa4c3c
Kanwei Li <kanwei@gmail.com>
2008-07-10 07:03:46 -0400

    Implemented SuffixArray

---
+++ lib/containers/suffix_array.rb
@@ -0,0 +1,51 @@
+module Containers
+=begin rdoc
+    A suffix array enables fast substring search of a given string. An array of all possible substrings
+    is constructed and stored, and a binary search is then done to find a desired substring among those
+    stored. While more storage (and thus memory) is needed to create the SuffixArray, the advantage is
+    that substrings can be found in O(m log n) time, where m is the length of the substring to search for
+    and n is the total number of substrings.
+
+=end
+  class SuffixArray
+    # Creates a new SuffixArray with a given string. Object of any class implementing a #to_s method can
+    # be passed in, such as integers.
+    #
+    #   s_array = Containers::SuffixArray("abracadabra")
+    #   s_array["abra"] #=> true
+    #
+    #   number = Containers::SuffixArray(1234567)
+    #   number[1] #=> true
+    #   number[13] #=> false 
+    def initialize(string)
+      string = string.to_s
+      raise "SuffixArray needs to be initialized with a non-empty string" if string.empty?
+      @original_string = string
+      @suffixes = []
+      string.size.times.each do |i|
+        @suffixes << string[i..-1]
+      end
+
+      # Sort the suffixes in ascending order
+      @suffixes.sort! { |x, y| x <=> y }
+    end
+    
+    # Returns true if the substring occurs in the string, false otherwise.
+    def has_substring?(substring)
+      substring = substring.to_s
+      substring_length = substring.length-1
+      l, r = 0, @suffixes.size-1
+      while(l <= r)
+        mid = (l + r) / 2
+        suffix = @suffixes[mid][0..substring_length]
+        case substring <=> suffix
+        when 0 then return true
+        when 1 then l = mid + 1
+        when -1 then r = mid - 1
+        end
+      end
+      return false
+    end
+    alias :[] :has_substring?
+  end
+end
---
+++ spec/suffix_array_spec.rb
@@ -0,0 +1,40 @@
+require 'lib/containers/suffix_array'
+
+describe Containers::SuffixArray do
+  describe "(empty)" do
+    it "should not initialize with empty string" do
+      lambda { Containers::SuffixArray.new("") }.should raise_error
+    end
+  end
+  
+  describe "(non-empty)" do
+    before(:each) do
+      @s_array = Containers::SuffixArray.new("abracadabra")
+    end
+  
+    it "should has_substring? each possible substring" do
+      @s_array.has_substring?("a").should eql(true)
+      @s_array.has_substring?("abra").should eql(true)
+      @s_array.has_substring?("abracadabra").should eql(true)
+      @s_array.has_substring?("acadabra").should eql(true)
+      @s_array.has_substring?("adabra").should eql(true)
+      @s_array.has_substring?("bra").should eql(true)
+      @s_array.has_substring?("bracadabra").should eql(true)
+      @s_array.has_substring?("cadabra").should eql(true)
+      @s_array.has_substring?("dabra").should eql(true)
+      @s_array.has_substring?("ra").should eql(true)
+      @s_array.has_substring?("racadabra").should eql(true)
+    end
+    
+    it "should not has_substring? substrings it does not have" do
+      @s_array.has_substring?("nope").should eql(false)
+    end
+    
+    it "should work with numbers (calls to_s)" do
+      number = Containers::SuffixArray.new(123456789)
+      number[1].should eql(true)
+      number.has_substring?(12).should eql(true)
+      number.has_substring?(13).should eql(false)
+    end
+  end
+end
