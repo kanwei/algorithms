2f8d03a3b87e522b2c3d65c1bb2a7c43657331dc
Kanwei Li <kanwei@gmail.com>
2009-03-27 18:34:32 -0400

    fix gc marking specs

--- spec/deque_gc_mark_spec.rb
+++ spec/deque_gc_mark_spec.rb
@@ -4,13 +4,15 @@ require 'algorithms'
 if defined? Containers::CDeque
   describe "CDeque" do
     it "should mark ruby object references" do
-      anonymous_class = Class.new
+      anon_class = Class.new
       @deque = Containers::CDeque.new
-      100.times { @deque.push_front(anonymous_class.new) }
+      100.times { @deque.push_front(anon_class.new) }
       # Mark and sweep
       ObjectSpace.garbage_collect
       # Check if any instances were swept
-      ObjectSpace.each_object(anonymous_class).count.should eql(100)
+      count = 0
+      ObjectSpace.each_object(anon_class) { |x| count += 1 }
+      count.should eql(100)
     end
   end
 end
