ec0c03440640c5fe1c134d1714c247d5c5fa869a
Amit Thawait <amit17thawait@gmail.com>
2015-11-19 11:21:14 +0530

    Explicit return not required for containers containing < 2 elements

--- lib/algorithms/sort.rb
+++ lib/algorithms/sort.rb
@@ -98,7 +98,6 @@ module Algorithms::Sort
   # 
   #   Algorithms::Sort.insertion_sort [5, 4, 3, 1, 2] => [1, 2, 3, 4, 5]
   def self.insertion_sort(container)
-    return container if container.size < 2
     (1..container.size-1).each do |i|
       value = container[i]
       j = i-1
