8c9ed0ba96c60606602ddc2648b6ce12de7d0d2c
nyapsilon <maximbarsukov@bk.ru>
2021-04-17 18:01:01 +0300

    Trailing whitespaces removed at benchmarks

--- benchmarks/deque.rb
+++ benchmarks/deque.rb
@@ -9,7 +9,7 @@ RBench.run(2) do
   %w(array deque).each { |s| self.send(:column, s.intern) }
   deque = Containers::Deque.new
   array = []
-  
+
   report "Insertion at end" do
     array { 1000000.times  { |x| array << x } }
     deque { 1000000.times  { |x| deque.push_back(x) } }
--- benchmarks/treemaps.rb
+++ benchmarks/treemaps.rb
@@ -8,38 +8,38 @@ require 'rbench'
 RBench.run(2) do
   trees = %w(hash rbtree splaytree)
   trees.each { |tree| self.send(:column, tree.intern) }
-  
+
   rbtree = RBTreeMap.new
   splaytree = SplayTreeMap.new
   hash = Hash.new
-  
+
   random_array = Array.new(300000) { |i| rand(i) }
-  
+
   report "Insertion" do
     rbtree { random_array.each_with_index  { |x,index| rbtree[index] = x } }
     splaytree { random_array.each_with_index  { |x,index| splaytree[index] = x } }
     hash { random_array.each_with_index  { |x,index| hash[index] = x } }
   end
-  
+
   report "has_key? (linear order)" do
     rbtree { random_array.each { |n| rbtree.has_key?(n) } }
     splaytree { random_array.each { |n| splaytree.has_key?(n) } }
     hash { random_array.each { |n| hash.has_key?(n) } }
   end
-  
+
   report "Lookup in sorted order" do
     rbtree { rbtree.each { |k, v| k } }
     splaytree { splaytree.each { |k, v| k } }
     hash { hash.sort.each { |k, v| k } }
-    
+
     # a1, a2, a3 = [], [], []
     # rbtree.each { |k, v| a1 << k }
     # splaytree.each { |k, v| a2 << k }
     # hash.sort.each { |k, v| a3 << k }
-    # 
+    #
     # puts "Lookup correct" if a1 == a2 && a1 == a3
   end
-  
+
   report "Random lookups in a smaller subset" do
     select_subset = random_array[0..random_array.size/20] # 5%
     size = select_subset.size
@@ -47,5 +47,5 @@ RBench.run(2) do
     splaytree { 10000.times { splaytree[ select_subset[rand(size)] ] } }
     hash { 10000.times { hash[ select_subset[rand(size)] ] } }
   end
-  
+
 end
